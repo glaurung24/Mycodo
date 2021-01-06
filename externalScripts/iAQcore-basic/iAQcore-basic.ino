a09d9ed9d7c83286db5afe88ea91420bc8edf105
andreas <andreas.theiler@physics.uu.se>
2021-01-06 21:28:36 +0100

    (at) added test libraries for the HIH70xx and iaq-core sensors.

---
+++ externalScripts/iAQcore-basic/iAQcore-basic.ino
@@ -0,0 +1,55 @@
+/*
+  iAQcore-basic.ino - basic demo sketch (no error handling) printing results of the iAQ-core indoor air quality sensor module with I2C interface from ams.
+  Created by Maarten Pennings 2017 Dec 9
+*/
+
+
+#include <Wire.h>   // I2C library
+#include "iAQcore.h"
+
+
+iAQcore iaqcore;
+
+
+void setup() {
+  // Enable serial
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("Starting iAQ-Core basic demo");
+
+  // Enable I2C for ESP8266 NodeMCU boards [VDD to 3V3, GND to GND, SDA to D2, SCL to D1]
+  int sda = 4;
+  int scl = 5;
+  Wire.begin(sda, scl);
+  Wire.setClockStretchLimit(1000); // Default is 230us, see line78 of https://github.com/esp8266/Arduino/blob/master/cores/esp8266/core_esp8266_si2c.c
+  
+  // Enable I2C for Arduino pro mini or Nano [VDD to VCC/3V3, GND to GND, SDA to A4, SCL to A5]
+  //Wire.begin(); 
+  
+  // Enable I2C for PlatformIO
+  //Wire.setSDA(SDA); Wire.setSCL(SCL); Wire.begin();
+
+  // Enable iAQ-Core
+  iaqcore.begin();
+}
+
+
+void loop() {
+  // Read
+  uint16_t eco2;
+  uint16_t stat;
+  uint32_t resist;
+  uint16_t etvoc;
+  iaqcore.read(&eco2,&stat,&resist,&etvoc);
+  
+  // Print
+  Serial.print("iAQcore: ");
+  Serial.print("eco2=");   Serial.print(eco2);     Serial.print(" ppm,  ");
+  Serial.print("stat=0x"); Serial.print(stat,HEX); Serial.print(",  ");
+  Serial.print("resist="); Serial.print(resist);   Serial.print(" ohm,  ");
+  Serial.print("tvoc=");   Serial.print(etvoc);    Serial.print(" ppb");
+  Serial.println();
+  
+  // Wait
+  delay(1000); 
+}
